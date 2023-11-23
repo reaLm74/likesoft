793fbacf0b325f5f9a6045bca9138452c6e0035d
Евгений <geoche74@gmail.com>
2023-11-23 07:39:44 +0500

    Add authenticated users

--- likesoft/likesoft/settings.py
+++ likesoft/likesoft/settings.py
@@ -118,8 +118,7 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated', ВЕНУТЬ!!!
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
